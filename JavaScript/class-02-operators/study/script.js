d20aba8067834bab52660fb784f1a59a2094ef37
Jose Murilo <murilojucas360@hotmail.com>
2022-11-08 16:01:00 -0300

    add class-operators

---
+++ JavaScript/class-02-operators/study/script.js
@@ -0,0 +1,62 @@
+let num1 = 3
+let num2 = 3
+
+// console.log(num1 + num2)
+// console.log(num1 - num2)
+// console.log(num1 * num2)
+// console.log(num1 / num2)
+// console.log(num1 % num2)
+// console.log(num1 ** num2)
+// console.log(25 ** (1/2)) // Raiz Quadrada
+
+// console.log(++num1) // Pré encremento
+// console.log(--num1) // Pré encremento
+
+// console.log(num1++) // Pos encremento Ou seja na linha seguinte
+// console.log(num1--) // Pos encremento
+// console.log(num1);
+
+
+// >>>> Operador de atribuição <<<<
+
+// num1 = num1 + 2
+// num1 += 3
+// num1 -= 2
+// num1 *= 3
+// num1 /= 3
+// num1 %= 4
+// console.log(num1)
+
+
+// >>>> Comparasion Operators (Boolean) <<<<
+
+// console.log(num1 > num2)
+// console.log(num1 < num2)
+// console.log(num1 <= num2)
+// console.log(num1 >= num2)
+// console.log(num1 != num2)
+
+
+// console.log(num1 == num2)
+// console.log(num1 === num2)
+
+
+
+// >>>> Logical Operators (Boolean) <<<<
+
+// and &&
+// or ||
+// not !
+
+let economizarDinheiro = true
+let juntarDinheiro = true
+
+// console.log(economizarDinheiro && juntarDinheiro)
+// console.log(!economizarDinheiro && juntarDinheiro)
+
+
+let guardarSelic = true;
+let pegarEmprestimo = false;
+
+// console.log(guardarSelic || pegarEmprestimo)
+// console.log(!guardarSelic) // Negar alguma coisa !
