3dc45206c4299768106aea91cf1a78bac6e5c97f
Jose Murilo <murilojucas360@hotmail.com>
2022-11-16 16:31:19 -0300

    add class-04 data-manipulation

---
+++ JavaScript/class-04-data-manipulation/study/script.js
@@ -0,0 +1,64 @@
+// >>>>> Data Manipulation <<<<<
+
+// Prototype (Chain)
+
+function Car(motorPower) {
+    this.motor = motorPower
+}
+
+// console.log(new Car("v8").motor)
+
+let myName = "Murilo"
+let age = 16
+let phrase = "Eu sou Web Developer"
+
+// console.log(myName.__proto__)
+// console.log(age)
+
+// >>>>> Strings <<<<<
+
+
+// console.log(myName.length)
+// console.log(myName[1])
+// console.log(myName.toLowerCase())
+// console.log(myName.toLocaleUpperCase())
+
+// console.log(phrase.split(","))
+// console.log(phrase.split(" ").join("_"))
+
+// console.log("Web" == "web")
+// console.log(phrase.includes("Web"))
+// console.log(phrase.toLocaleLowerCase().includes("web"))
+// console.log(phrase.toLocaleUpperCase().includes("WEB"))
+
+
+// <<<<< Numbers >>>>>
+
+let num = 789
+let num1 = 7.287
+let num2 = -7.9876
+
+// console.log(String(num).length)
+// console.log(String(num1).replace(".", "").length)
+// console.log(String(num1.toFixed()).replace(".", ","))
+// console.log(Number(num1).toFixed())
+
+// Convert Currency (International Standard Currency Code ISO 4217)
+// console.log(num1.toLocaleString("pt-BR")) // Boa pratica
+// console.log(num1.toLocaleString("pt-BR", {style: "currency", currency: "brl"}))
+// console.log(num1.toLocaleString("pt-BR", {style: "currency", currency: "usd"}))
+// console.log(num1.toLocaleString("pt-BR", {style: "currency", currency: "eur"}))
+
+
+console.log(Math.PI)
+console.log(Math.sqrt(25))
+console.log(Math.abs(num2))
+
+console.log(Math.min(2, 64, 3, 764, 85, 869, 77))
+console.log(Math.max(2, 64, 3, 764, 85, 869, 77))
+
+console.log(Math.ceil(num1))
+console.log(Math.round(num1))
+console.log(Math.floor(num1))
+
+console.log(Math.round(Math.random().toFixed(1) * 10)) // IMPORTANTE DE MAIS
