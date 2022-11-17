ee6a454063ee36de149eb028db47529793dc9543
Jose Murilo <murilojucas360@hotmail.com>
2022-11-17 16:34:02 -0300

    add class-04 challange data-manipulation

---
+++ JavaScript/class-04-data-manipulation/challange/script.js
@@ -0,0 +1,17 @@
+const digitalClock = document.querySelector("#clock")
+
+setInterval(() => {
+    let date = new Date()
+    let hour = date.getHours()
+    let minutes = date.getMinutes()
+    let seconds = date.getSeconds()
+    let milliseconds = date.getMilliseconds()
+
+    digitalClock.innerHTML = `${String(hour)
+    .padStart(2, "0")}: ${String(minutes)
+    .padStart(2, "0")}: ${String(seconds)
+    .padStart(2, "0")}: ${String(milliseconds)
+    .padStart(2, "0")} `
+            
+    console.log("A")
+}, 100);
