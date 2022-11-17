713a78707155b9321a50f31c6c203c6326b4bf7b
Jose Murilo <murilojucas360@hotmail.com>
2022-11-17 16:05:50 -0300

    add auteration Data-Manipulation

--- JavaScript/class-04-data-manipulation/study/script.js
+++ JavaScript/class-04-data-manipulation/study/script.js
@@ -50,15 +50,61 @@ let num2 = -7.9876
 // console.log(num1.toLocaleString("pt-BR", {style: "currency", currency: "eur"}))
 
 
-console.log(Math.PI)
-console.log(Math.sqrt(25))
-console.log(Math.abs(num2))
+// console.log(Math.PI)
+// console.log(Math.sqrt(25))
+// console.log(Math.abs(num2))
 
-console.log(Math.min(2, 64, 3, 764, 85, 869, 77))
-console.log(Math.max(2, 64, 3, 764, 85, 869, 77))
+// console.log(Math.min(2, 64, 3, 764, 85, 869, 77))
+// console.log(Math.max(2, 64, 3, 764, 85, 869, 77))
 
-console.log(Math.ceil(num1))
-console.log(Math.round(num1))
-console.log(Math.floor(num1))
+// console.log(Math.ceil(num1))
+// console.log(Math.round(num1))
+// console.log(Math.floor(num1))
 
-console.log(Math.round(Math.random().toFixed(1) * 10)) // IMPORTANTE DE MAIS
+// console.log(Math.round(Math.random().toFixed(1) * 10)) // IMPORTANTE DE MAIS
+
+
+
+// >>>>> Date Manipulation <<<<<
+
+let date = new Date()
+let date1 = new Date("2022-03-01 12:03:19")
+let date2 = new Date("2022-04-01 15:03:19")
+
+// document.write(date)
+// let hour = date1.getHours() // Para horas
+// let minutes = date1.getMinutes() // Para minutos
+// let seconds = date1.getSeconds() // Para segundos
+
+// document.write(`${String(hour).padStart(2, "0")}: ${String(minutes).padStart(2, "0")}: ${String(seconds).padStart(2, "0")}`)
+
+// date1.setHours(date.getHours() - 3)
+// date1.setMinutes(date.getMinutes() + 60)
+// date1.setSeconds(date.getSeconds() + 60)
+
+// document.write(date1.toLocaleTimeString("pt-BR"))
+
+
+
+let day = date.getDay() // Para Dia
+let data = date.getDate() // Para Mês
+let month = date.getMonth() + 1 // Para Mês
+let year = date.getFullYear() // Para Ano
+// let minuts = date.getMinutes() // Para Ano
+// let seconds = date.getSeconds() // Para Ano
+
+// document.write(day + 1) // 0 - 6
+// document.write(data) 
+// document.write(month + 1) // 0 - 12
+// document.write(year)
+// document.write(`${String(data).padStart(2, "0")}/${(String(month).padStart(2, "0"))}/${year}`)
+
+// date.setDate((date.getDay() + 30))
+date.setDate((date.getMonth() + 2))
+date.setDate((date.getFullYear() + 30))
+
+// document.write(date1.toLocaleString("pt-BR"))
+
+// document.write(date1.toLocaleString("pt-BR"))
+
+document.write(Math.floor((date2 - date1) / 1000 / 60 / 60 / 24))
