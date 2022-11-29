c5670bc563e4ed9951c55b1c71029247a7e1c72b
Jose Murilo <murilojucas360@hotmail.com>
2022-11-29 14:45:10 -0300

    add class-06 study

--- JavaScript/class-06-looping/study/script.js
+++ JavaScript/class-06-looping/study/script.js
@@ -2,7 +2,7 @@
 
 // While
 
-let count = 0
+// let count = 0
 // while (count <= 500) {
 //     if (count % 2 == 0) {
 //         console.log(count)
@@ -41,17 +41,48 @@ let count = 0
 //     } while (count <= 10);
 
 
+// outro exemplo
+
+// let resp = true
+// while (resp) {
+//     resp = prompt("Digite seu nome: ")
+// }
+
 
 // For
 
-for (let count = 0; count <= 10; count++) { // increment factor
-    if (count > 2 && count < 8) {
-        count ++ // Caution, two increments!
-        continue
-        // break
-    }
+// for (let count = 0; count <= 10; count++) { // increment factor
+//     if (count > 2 && count < 8) {
+//         count ++ // Caution, two increments!
+//         continue
+//         // break
+//     }
 
-    if (count % 2 == 0) {
-        console.log(count)
-    }
-}
+//     if (count % 2 == 0) {
+//         console.log(count)
+//     }
+// }
+
+
+// For-of
+// forof = Trabalha com o valor do array.
+
+// const array = [1, 2, 3, 4, 5]
+// for (let count = 0; count < array.length; count++) {
+//     const element = array[count];
+//     console.log(element)
+// }
+
+// for (const valor of array) {
+//     console.log(valor)
+// }
+
+
+// For-in
+
+// const array = ["A", "B", "C", "D", "E"]
+// for (const index in array) {
+//     console.log(array[index])
+// }
+
+// iterator = looping
