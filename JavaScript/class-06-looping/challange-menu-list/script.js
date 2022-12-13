2d33d060925be4bb2ac2716f4060c98601f39e3f
Jose Murilo <murilojucas360@hotmail.com>
2022-12-13 15:44:57 -0300

    add challange menu-list complete

--- JavaScript/class-06-looping/challange-menu-list/script.js
+++ JavaScript/class-06-looping/challange-menu-list/script.js
@@ -12,11 +12,11 @@
        OK - Alertar caso a lista esteja vazia
 
     3 - Remover item cadastrado
-        - Alertar caso a lista esteja vazia
-        - Poder remover itens pelo nome enquanto não cancelar
-        - Remover item automaticamente caso só tenha um na lista
-        - Mostrar mensagem de sucesso junto do nome ao remover
-        - Alertar usuário caso o item informado não seja encontrado
+       OK - Alertar caso a lista esteja vazia
+       OK - Poder remover itens pelo nome enquanto não cancelar
+       OK - Remover item automaticamente caso só tenha um na lista
+       OK - Mostrar mensagem de sucesso junto do nome ao remover
+       OK - Alertar usuário caso o item informado não seja encontrado
 
     4 - Sair do programa
        OK - Sair do programa quando escolher a opção 4 ou cancelar
@@ -36,30 +36,30 @@ while (true) {
     `)
 
     switch (menuOption) {
-        case "1": 
-        while (true) {
-            let item = prompt("Digite o nome do item")
+        case "1":
+            while (true) {
+                let item = prompt("Digite o nome do item")
 
-            if (item === null) {
-                break;
-            }
-        
-            if (item === '') {
-                alert("Digite um nome para cadastrar")
-                continue;
-            }
+                if (item === null) {
+                    break;
+                }
 
-            if (listItens.includes(item)) {
-                alert("Item já cadastrado")    
-            } else {
-                listItens.push(item)
-                alert(`"${item}" cadastrado com sucesso`)
+                if (item === '') {
+                    alert("Digite um nome para cadastrar")
+                    continue;
+                }
+
+                if (listItens.includes(item)) {
+                    alert("Item já cadastrado")
+                } else {
+                    listItens.push(item)
+                    alert(`"${item}" cadastrado com sucesso`)
+                }
             }
-        }
             break;
 
         case "2":
-            if(listItens.length == 0) {
+            if (listItens.length == 0) {
                 alert("Lista vazia!")
             } else {
                 alert(listItens)
@@ -67,7 +67,36 @@ while (true) {
             break;
 
         case "3":
-            
+            if (listItens.length == 1){
+                alert(`Item "${listItens[0]}" foi removido com sucesso!`)
+                listItens.pop()
+                break;
+            }
+
+            while (true) {
+                if (listItens.length == 0) {
+                    alert("Lista vazia!")
+                    break;
+                } else {
+                    let item = prompt("Digite o nome para cadastrar")
+
+                    if (item === null) {
+                        break;
+                    }
+
+                    if (item === '') {
+                        alert("Digite um nome para cadastrar")
+                        continue;
+                    }
+
+                    if (listItens.includes(item)) {
+                        listItens.splice(listItens.indexOf(item), 1)
+                        alert(`"${item}" foi removido`)
+                    } else {
+                        alert("Este item não encontrado")
+                    }
+                }
+            }
             break;
 
         default:
