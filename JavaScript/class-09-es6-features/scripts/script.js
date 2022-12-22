b87a7b1d44bfee9a9c66d282b49e0aee4c41c07b
Jose Murilo <murilojucas360@hotmail.com>
2022-12-22 15:41:15 -0300

    add class 09 study - complete

--- JavaScript/class-09-es6-features/scripts/script.js
+++ JavaScript/class-09-es6-features/scripts/script.js
@@ -1,18 +1,18 @@
 // Modules (import & export)
 // import { sum, sub, mult, div } from "./functions.js";
 
-import { operations } from "./functions.js";
+// import { operations } from "./functions.js";
 
-console.log('Principal');
+// console.log('Principal');
 
-let num1 = 12
-let num2 = 33
+// let num1 = 12
+// let num2 = 33
 
 
-operations.sum(num1, num2)
-operations.sub(num1, num2)
-operations.mult(num1, num2)
-operations.div(num1, num2)
+// operations.sum(num1, num2)
+// operations.sub(num1, num2)
+// operations.mult(num1, num2)
+// operations.div(num1, num2)
 
 
 // sum(num1, num2)
@@ -47,23 +47,81 @@ operations.div(num1, num2)
 // console.log(person1.canDrive)
 // console.log(person1.status())
 
-// >>>>>> Factory <<<<<<<<
+// //>>>>>>> Factory (Creational Design Pattern ) <<<<<<<<<<
 
-function person(name, age) {
-    return {
-        name,
-        age,
-        canDrive: age >= 18,
-        status: function status(canDrive) {
-            if (this.canDrive) {
-                return "Pode dirigir"
-            } else {
-                return "Não pode dirigir"
-            }
-        }
-    }
-}
+// function person(name, age) {
+//     return {
+//         name,
+//         age,
+//         canDrive: age >= 18,
+//         status: function status(canDrive) {
+//             if (this.canDrive) {
+//                 return "Pode dirigir";
+//             } else {
+//                 return "Não pode dirigir";
+//             }
+//         }
+//     }
+// }
 
-const person1 = person("Murilo", 16)
+// const person2 = person("José", 18)
+// console.log(person2.status());
 
-console.log(person1.status());
+
+// >>>>>>> Destructure <<<<<<<
+
+// const array = ["A", "B"]
+// const [item1, item2] = array
+// console.log(array);
+// console.log(item1);
+// console.log(item2);
+
+// >>>>>>> Rest Operator <<<<<<<
+
+// function showArray(...array) {
+//     console.log(array);
+//     // console.log(second);
+// }
+// showArray(2, 4, 5, 6, 7)
+
+// function showArray(array, ...numbers) {
+//     console.log(array);
+//     console.log(numbers);
+// }
+
+// showArray(array, 4, 5, 6, 7)
+
+
+// >>>>>>> Spread Operator <<<<<<<
+
+// const numbers = [2, 3, 4, 6, 5]
+// const clone = [...numbers] // Clona
+// clone.pop()
+// const [...clone2] = numbers // Ele clona também
+
+// console.log(numbers);
+// console.log(clone);
+// console.log(clone2);
+// console.log(...numbers);
+// console.log(Math.max(...numbers));
+// console.log(Math.max('1', '2', '3'));
+
+
+// >>>>>>> Exemplo <<<<<<<
+
+const list = document.querySelectorAll('div')
+console.log(list);
+
+// list.forEach((value) => console.log(value))
+
+[...list].map((value) => console.log(value))
+
+let number; number = 7
+console.log(number);
+
+// ASI (Automatic Semicolon Insertion)
+
+// Babel (ES6 to ES5)
+// - Parser: Code 5 to AST (Abstract Syntex Tree)
+// - Transformer: AST Manipulates (JSON)
+// - Generator: AST to Code 5
