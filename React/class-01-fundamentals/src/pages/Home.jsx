9f18334fa833cd0846e314b196756083a2394e3c
Jose Murilo <murilojucas360@hotmail.com>
2023-01-02 16:46:46 -0300

    add study class-react-fundamentals

---
+++ React/class-01-fundamentals/src/pages/Home.jsx
@@ -0,0 +1,27 @@
+
+
+export function Home() {
+  return (
+    <div>
+      <header>
+        <h1>Class React - Fundamentals</h1>
+      </header>
+
+      <main>
+        <article>
+          <section>
+            <h2>Paragraph 1</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti et vel, amet alias saepe dolor accusantium, quibusdam magnam quo aliquam at. Aliquam perspiciatis minima ipsa commodi fugiat pariatur eum magni.
+            </p>
+          </section>
+
+          <section>
+            <h2>Paragraph 2</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt rem sed, dolores eaque sequi illum distinctio, ratione neque voluptates ipsa reiciendis culpa labore quisquam doloremque dolorem et commodi. Facilis, iure.</p>
+          </section>
+        </article>
+      </main>
+    </div>
+  )
+}
