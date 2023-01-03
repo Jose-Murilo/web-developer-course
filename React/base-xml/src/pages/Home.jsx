157a480f20c0af60805e6ebdb6c4661b85395725
Jose Murilo <murilojucas360@hotmail.com>
2023-01-03 16:24:45 -0300

    add class-react base-xml

---
+++ React/base-xml/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import './Home.css'
+import IconImage from"../images/react.svg"
+
+export function Home() {
+  return (
+    <div className='container'>
+      <h1>Class React Base Xml</h1>
+      <img src={IconImage} alt='Imagem do react'/>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum, ut? Asperiores quibusdam aperiam repellendus aliquam unde libero praesentium rerum harum earum amet, aliquid illum, pariatur, aut animi! Doloremque, illo fuga?
+      </p>
+    </div>
+  )
+}
