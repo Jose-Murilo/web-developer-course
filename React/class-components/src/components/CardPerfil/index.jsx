ff558d560bfde7b266b246f5f178663cf03d401a
Jose Murilo <murilojucas360@hotmail.com>
2023-01-04 16:57:37 -0300

    add study class-components react

--- React/class-components/src/components/CardPerfil/index.jsx
+++ React/class-components/src/components/CardPerfil/index.jsx
@@ -1,11 +1,28 @@
 import './style.css'
+import React from 'react'
 
-export function CardPerfil({login, name, age}) {
-    return(
-        <div className="containerCard">
-            <img src={`https://github.com/${login}.png`} alt="image-user" />
-            <p>Nome: {name}</p>
-            <p>Idade: {age}</p>
-        </div>
-    )
+// Funcional
+
+// export function CardPerfil({login, name, age}) {
+//     return(
+//         <div className="containerCard">
+//             <img src={`https://github.com/${login}.png`} alt="image-user" />
+//             <p>Nome: {name}</p>
+//             <p>Idade: {age}</p>
+//         </div>
+//     )
+// }
+
+
+// com Class
+export class CardPerfil extends React.Component {
+    render() {
+        return (
+            <div className="containerCard">
+                <img src={`https://github.com/${this.props.login}.png`} alt="image-user" />
+                <p>Nome: {this.props.name}</p>
+                <p>Idade: {this.props.age}</p>
+            </div>
+        )
+    }
 }
