6dbe8a1791df5697ce236e2a1233b0c1200eaca0
Jose Murilo <murilojucas360@hotmail.com>
2023-01-04 15:52:40 -0300

    add folder components react

---
+++ React/class-components/src/components/CardPerfil/index.jsx
@@ -0,0 +1,10 @@
+import './style.css'
+export function CardPerfil() {
+    return(
+        <div className="containerCard">
+            <img src="https://github.com/Jose-Murilo.png" alt="Image-user" />
+            <p>Nome: {'Murilo'}</p>
+            <p>Idade: {16}</p>
+        </div>
+    )
+}
