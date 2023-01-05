b75cb99d2156ebbc50ce755fe15052fe762b7652
Jose Murilo <murilojucas360@hotmail.com>
2023-01-05 14:43:40 -0300

    add study class-components react

--- React/class-components/src/pages/App.jsx
+++ React/class-components/src/pages/App.jsx
@@ -4,23 +4,23 @@ import {CardPerfil} from '../components/CardPerfil/index'
 export function App() {
 
   return (
-    <div className="container">
+    <div className="App">
         <h1>Class components</h1>
         <CardPerfil 
           name = 'Murilo' 
-          age = {16} 
+          age ='16' 
           login ='Jose-Murilo'
         />
 
         <CardPerfil 
           name = 'Italo' 
-          age = {25} 
+          age ='25' 
           login ='Italo-Morais'
         />
-
+        
         <CardPerfil 
           name = 'EmanuelQuintino' 
-          age = {29} 
+          age ='29' 
           login ='EmanuelQuintino'
         />
     </div>
