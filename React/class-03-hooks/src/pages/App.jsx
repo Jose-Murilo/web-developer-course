b94cce4eec02554d8d3d1240182342bdfd65bfaf
Jose Murilo <murilojucas360@hotmail.com>
2023-01-06 15:58:39 -0300

    add study class-react-hooks completed

--- React/class-03-hooks/src/pages/App.jsx
+++ React/class-03-hooks/src/pages/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 export function App() {
@@ -7,12 +7,21 @@ export function App() {
   // const [count, setCount] = useState(0);
   const [text1, setText] = useState('')
   const [text2, setText2] = useState('')
-
+  const [data, setData] = useState('')
   // let count = 0
+
+  useEffect(() => {
+    if (text2) {
+      fetch(`https://api.github.com/users/${text2}`)
+        .then(response => response.json())
+        .then(data => setData(data))
+    }
+  }, [text2])
+
   return (
     <div className="container">
-      <h1>Class Hooks</h1>
-      {/* <p>Nome: {name}</p>
+      {/* <h1>Class Hooks</h1>
+      <p>Nome: {name}</p>
       <p>Número: {count}</p>
       <button onClick={() => {
         //  count += 1
@@ -23,12 +32,18 @@ export function App() {
       <form action="">
         <p>Palavra: {text1}</p>
         <p>Palavra: {text2}</p>
-        <input type="text" placeholder='Digite alguma coisa' onChange={(event) => setText(event.target.value)} />
+        <input type="text" placeholder='Digite alguma coisa' onChange={(event) => setText((event.target.value))} />
         <button onClick={(event) => {
           event.preventDefault()
           setText2(text1)
         }}>Enviar</button>
       </form>
+
+      <section>
+        <h2>{data.name}</h2>
+        <img src={data.avatar_url} alt="" />
+        <p>{data.bio}</p>
+      </section>
     </div>
   )
 }
